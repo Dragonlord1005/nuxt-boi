9ce28ba7b54b2f48821b23a24893a75daa1a6665
Dragonlord1005 <68302450+Dragonlord1005@users.noreply.github.com>
2022-04-29 19:58:41 +0000

    sup

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -9,7 +9,6 @@
         </label>
         <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
           <li><a>Homepage</a></li>
-          <li><a>Portfolio</a></li>
           <li><a>About</a></li>
         </ul>
       </div>
@@ -19,7 +18,7 @@
     </div>
     <div class="navbar-end">
       <button class="btn btn-ghost btn-circle">
-        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+        <svg xmlns="public/favicon.ico" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
       </button>
       <button class="btn btn-ghost btn-circle">
         <div class="indicator">
