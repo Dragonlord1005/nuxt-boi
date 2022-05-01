b9651f4ef5a9c3188f5fe12ce25ae24b7b481ade
Dragonlord1005 <68302450+Dragonlord1005@users.noreply.github.com>
2022-05-01 17:02:39 +0000

    help me

--- pages/index.vue
+++ pages/index.vue
@@ -2,8 +2,5 @@
   <div>
     <h1 class="flex justify-center font-bold text-4xl">Hi there</h1>
     <p>It's working so far</p>
-    <NuxtLink to="/about">
-      <button class="btn">Hi</button>
-    </NuxtLink>
   </div>
 </template>
