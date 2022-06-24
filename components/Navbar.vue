fa484de93f63767af84da86c24d88041f0fe4751
Drago <68302450+Dragonlord1005@users.noreply.github.com>
2022-06-24 08:53:35 -0600

    gotta center it

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,21 +1,39 @@
 <!-- We need a nuxt 3 navbar -->
 <!-- We need it to have a menu with links to the different pages. -->
 <!-- It needs to be responsive -->
-<!-- We will add links and names using arguments as an array -->
 <!-- We will also use tailwindcss to style the navbar. -->
-<!-- we will be using daisyui for navbar components -->
 <!-- It will also be in typescript -->
+<!-- Because we are using nuxt 3, we will be using nuxtLink for routing. -->
+<!-- The title will be centered -->
+<!-- The navigation menu will be to the left of the title and will be in a dropdown hamburger menu. -->
+<!-- The whole navbar will be a container and will be in the header. -->
 <script lang="ts">
+
 </script>
 
 <template>
-  <div>
-    <div class="navbar bg-base-100">
-      <!-- <div class="navbar-center"> -->
-      <a class="btn btn-ghost">daisyui</a>
-      <!-- </div> -->
+  <header class="bg-gray-900">
+    <div class="container flex mx-auto px-4 py-16 flex flex-col lg:flex-row justify">
+
+      <!-- Navigation -->
+      <div class="flex-1">
+        <nuxtLink to="/" class="text-white text-lg font-semibold pr-3 hover:text-yellow-300">
+          Home
+        </nuxtLink>
+        <nuxtLink to="/about" class="text-white text-lg font-semibold hover:text-yellow-300">
+          About
+        </nuxtLink>
+      </div>
+
+      <!-- Title -->
+      <!-- It must be in the center of the navbar -->
+      <div class="flex-1 justify-center mx-auto">
+        <h1 class="text-white text-4xl font-semibold">
+          Nuxt 3
+        </h1>
+        </div>
     </div>
-  </div>
+  </header>
 </template>
 
 <style scoped>
