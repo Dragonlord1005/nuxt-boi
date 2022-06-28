ec44aa669458bf7c86493aa127e3d7647f941908
Drago <68302450+Dragonlord1005@users.noreply.github.com>
2022-06-28 01:17:58 -0600

    Worked on the post some more

--- pages/posts/First-Post.vue
+++ pages/posts/First-Post.vue
@@ -13,6 +13,13 @@
             <article>
                 <p>Hi, this will be my first ever post on this sight I am building.</p>
                 <p>So far it's been going pretty well.</p>
+                <p>I've also switched to pnpm for package managing, and after configuiring it, it's been going pretty freaking good my mans!</p>
+                <p>The great thing about pnpm is that like, it's so fast and uses so little disk space.</p>
+                <p>Heres a link if you want to check it out.<NuxtLink to="https:\\pnpm.io" target="_blank">pnpm</NuxtLink></p>
+                <p>I've started making the switch from React/Next.js to Vue/Nuxt</p>
+                <p>I've been using tailwind as well, and instead of using a ui framework like I usually do, I'm actually making my own components!</p>
+                <p>Nuxt uses vue-router for routing which is nice</p>
+                <p>I've seen to run into the issue though where pages aren't rendering.</p>
             </article>
         </div>
     </div>
