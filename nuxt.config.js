990f3cfd22dd59fd2cb14f1f0ea7211e2e04fe32
Drago <68302450+Dragonlord1005@users.noreply.github.com>
2022-06-27 23:45:55 -0600

    dark

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,8 +2,11 @@ import { defineNuxtConfig } from "nuxt";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  buildModules: ["@nuxtjs/tailwindcss"],
+  buildModules: ["@nuxtjs/tailwindcss", "@nuxtjs/color-mode"],
   tailwindcss: {
     configPath: "tailwind.config.js",
   },
+  colorMode: {
+    classSuffix: ''
+  }
 });
