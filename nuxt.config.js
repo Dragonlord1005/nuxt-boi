2195c8eee0390c838daf647edb5caddc878c61dc
Drago <68302450+Dragonlord1005@users.noreply.github.com>
2022-06-29 17:22:08 -0600

    Windicss

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,12 +3,9 @@ import { defineNuxtConfig } from "nuxt";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  buildModules: ["@nuxtjs/tailwindcss", "@nuxtjs/color-mode"],
-  tailwindcss: {
-    configPath: "tailwind.config.js",
-  },
-  colorMode: {
-    classSuffix: ''
+  buildModules: ["nuxt-windicss"],
+  windicss: {
+    analyze: true
   },
   typescript: {
     shim: false,
